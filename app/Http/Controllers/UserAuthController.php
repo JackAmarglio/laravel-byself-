0f1817a6792f7726f5c4e23213a20877ae8a1c93
LeeMing-creator <90887612+LeeMing-creator@users.noreply.github.com>
2021-11-09 23:07:18 +0800

    Update UserAuthController.php

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -116,7 +116,6 @@ class UserAuthController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'email' => 'required|string|email|max:255',
-            // 'password' => 'required|string|min:6',
         ]);
         if ($validator->fails()) {
             return response(['errors' => $validator->errors()->all()], 422);
